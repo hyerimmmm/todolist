d37c8e7b8fe2170a0f4fcc6e9ba0ba0dd1abb42a
roqkfw <hyerim2888@naver.com>
2024-04-25 22:53:37 +0900

    Design: UI 디자인

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FormEvent, useCallback, useState } from "react";
+import styled from "styled-components";
+import GlobalStyle from "./GlobalStyle";
 
-function App() {
+type Props = {};
+
+const App = (props: Props) => {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  const onChange = useCallback((e: React.FormEvent<HTMLInputElement>) => {
+    setTodo(e.currentTarget.value);
+  }, []);
+  const AddList = useCallback(() => {}, []);
+
+  console.log(todo);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <TodolistPage>
+        <Container>
+          <Title>TODOLIST </Title>
+          <Wrapper>
+            <TodoInputWrapper>
+              <TodolistInput value={todo} onChange={onChange}></TodolistInput>
+              <AddBtn onClick={AddList}>+</AddBtn>
+            </TodoInputWrapper>
+          </Wrapper>
+        </Container>
+      </TodolistPage>
+    </>
   );
-}
+};
 
 export default App;
+
+const TodolistPage = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 10rem 30rem;
+  box-sizing: border-box;
+`;
+const Container = styled.div`
+  width: 100%;
+  background-color: #97cbeb;
+  height: 100%;
+  padding: 2rem 1rem 1rem 1rem;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+`;
+const Title = styled.div`
+  color: white;
+  font-weight: 600;
+  font-size: 2rem;
+`;
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  box-sizing: border-box;
+`;
+const TodolistInput = styled.input`
+  width: 100%;
+  border: 1px solid #57abdf;
+  padding: unset;
+  height: 2rem;
+`;
+const AddBtn = styled.button`
+  width: 2.5rem;
+  padding: unset;
+  background-color: #57abdf;
+  color: white;
+  border: unset;
+  font-size: 1.6rem;
+`;
+const TodoInputWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    html, body {
+height: 100%;
+    }
+    #root{
+        height: 100%;
+    }
+`;
+
+export default GlobalStyle;
